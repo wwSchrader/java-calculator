a50eb3c95fb9f4b5b7d113ab5779a47ab5cd7afc
wwSchrader <wwschrader@gmail.com>
2017-06-29 10:34:05 -0700

    Build static version of calculator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <InputDisplay mathOperation={this.props.mathOperation} />
+        <ChainedDisplay mathOperation={this.props.mathOperation} />
+        <Buttons />
       </div>
     );
+  }
+}
+
+class InputDisplay extends Component {
+  render() {
+    return (
+        <h2>{this.props.mathOperation[this.props.mathOperation.length - 1]}</h2>
+    );
+  }
+}
+
+class ChainedDisplay extends Component {
+  render() {
+    return (
+      <p>{this.props.mathOperation.join("")}</p>
+    );
+  }
+}
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <button>AC</button>
+          <button>CE</button>
+          <button>/</button>
+          <button>*</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>=</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var MATH_OPERATION = ['9', '+', '3'];
+
+ReactDOM.render(<App mathOperation={MATH_OPERATION}/>, document.getElementById('root'));
 registerServiceWorker();
