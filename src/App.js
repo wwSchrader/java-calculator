c49e376d7a3c4cf2799833669d39b050a49b82f5
wwSchrader <wwschrader@gmail.com>
2017-06-29 16:53:28 -0700

    Add methods for numeral inputs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mathOperation: ['9', '+', '4']
+    }
+    this.handleMathInput = this.handleMathInput.bind(this);
+  }
+
+  handleMathInput(mathInput) {
+    var mathArray;
+    switch(mathInput) {
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        mathArray = this.numeralInput(mathInput);
+      case '/':
+      case '*':
+      case '-':
+      case '+':
+        mathArray = this.operationInput(mathInput);
+    }
+    this.setState({
+      mathOperation: mathArray
+    });
+  }
+
+  numeralInput(mathInput) {
+    var mathArray = this.state.mathOperation;
+    var lastEntry = mathArray[mathArray.length - 1];
+    //if last input is an operator, push mathInput onto array
+    if (lastEntry === '/' || lastEntry === '*' || lastEntry === '-' || lastEntry === '+') {
+      mathArray.push(mathInput);
+    } else {
+      //add mathInput to last entry of mathArray
+      mathArray[mathArray.length - 1] = lastEntry.concat(mathInput);
+    }
+    return mathArray;
+  }
+
   render() {
     return (
       <div>
-        <InputDisplay mathOperation={this.props.mathOperation} />
-        <ChainedDisplay mathOperation={this.props.mathOperation} />
-        <Buttons />
+        <InputDisplay mathOperation={this.state.mathOperation} />
+        <ChainedDisplay mathOperation={this.state.mathOperation} />
+        <Buttons onMathInput={this.handleMathInput} />
       </div>
     );
   }
@@ -30,36 +75,45 @@ class ChainedDisplay extends Component {
 }
 
 class Buttons extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMathInput = this.handleMathInput.bind(this);
+  }
+
+  handleMathInput(e) {
+    this.props.onMathInput(e.target.value);
+  }
+
   render() {
     return (
       <div>
         <div>
-          <button>AC</button>
-          <button>CE</button>
-          <button>/</button>
-          <button>*</button>
+          <button value="AC" onClick={this.handleMathInput}>AC</button>
+          <button value="CE" onClick={this.handleMathInput}>CE</button>
+          <button value="/" onClick={this.handleMathInput}>/</button>
+          <button value="*" onClick={this.handleMathInput}>*</button>
         </div>
         <div>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>-</button>
+          <button value="7" onClick={this.handleMathInput}>7</button>
+          <button value="8" onClick={this.handleMathInput}>8</button>
+          <button value="9" onClick={this.handleMathInput}>9</button>
+          <button value="-" onClick={this.handleMathInput}>-</button>
         </div>
         <div>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
+          <button value="4" onClick={this.handleMathInput}>4</button>
+          <button value="5" onClick={this.handleMathInput}>5</button>
+          <button value="6" onClick={this.handleMathInput}>6</button>
+          <button value="7" onClick={this.handleMathInput}>+</button>
         </div>
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>=</button>
+          <button value="1" onClick={this.handleMathInput}>1</button>
+          <button value="2" onClick={this.handleMathInput}>2</button>
+          <button value="3" onClick={this.handleMathInput}>3</button>
+          <button value="=" onClick={this.handleMathInput}>=</button>
         </div>
         <div>
-          <button>0</button>
-          <button>.</button>
+          <button value="0" onClick={this.handleMathInput}>0</button>
+          <button value="." onClick={this.handleMathInput}>.</button>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var MATH_OPERATION = ['9', '+', '3'];
 
-ReactDOM.render(<App mathOperation={MATH_OPERATION}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
